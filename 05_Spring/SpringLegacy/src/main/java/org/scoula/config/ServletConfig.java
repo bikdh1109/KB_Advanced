a8054973d7451b2dd551add1f611a8f3f994db65
bikdh1109 <sking1109s@gmail.com>
2025-06-09 17:46:52 +0900

    SpringLegacy 업로드

---
+++ 05_Spring/SpringLegacy/src/main/java/org/scoula/config/ServletConfig.java
@@ -0,0 +1,29 @@
+package org.scoula.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@EnableWebMvc
+@ComponentScan(basePackages = "org.scoula.controller")
+public class ServletConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/resources/**") // URL이 /resources/로 시작하는 모든 경로
+                .addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);           // JSTL 지원 활성화
+        bean.setPrefix("/WEB-INF/views/");           // JSP 파일 기본 경로
+        bean.setSuffix(".jsp");                      // JSP 파일 확장자
+        registry.viewResolver(bean);                 // ViewResolver 등록
+    }
+}
